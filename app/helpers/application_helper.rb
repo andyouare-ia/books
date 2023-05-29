6196a77aeaac9a6001158d89f4143b7d0123a979
Nuria Toledano <ia.andyouare@gmail.com>
2023-05-29 17:08:16 +0100

    More React (changed to React 17 to follow guide) and Typescript config

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+  def react_component(component_name, props)
+    puts "react_component"
+    content_tag(
+      "div",
+      data: {
+        react_component: component_name,
+        props: props.to_json,
+      }
+    ) { "" }
+  end
 end
---
+++ app/javascript/packs/application.js
@@ -0,0 +1,3 @@
+import mount from "../mount";
+import Hello from "./hello_react";
+mount({ Hello });
